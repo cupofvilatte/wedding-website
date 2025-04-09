dfeb4c7b899282f3766614672ad5e18d4ea1c9eb
Vilate <viknapp22@gmail.com>
2025-04-09 14:47:26 -0600

    added base in vite config

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { resolve } from 'path';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
+  base: '/wedding-website/',
   root: 'src/',
   build: {
     outDir: '../dist',
